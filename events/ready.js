9b9c0466db75ddf885f1b80fb9fc2d073a88caea
RTXGAMIINGGG <160713982+RTXGAMIINGGG@users.noreply.github.com>
2024-02-21 13:57:06 +0530

    ready.js

--- events/ready.js
+++ events/ready.js
@@ -24,7 +24,7 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 const serverCount = client.guilds.cache.size;
 setInterval(() => client.user.setActivity({ 
   name:`Tickets`, 
-  type: ActivityType.Listening }), 10000);
+  type: ActivityType.Watching }), 10000);
 client.errorLog = config.errorLog
   
 }
